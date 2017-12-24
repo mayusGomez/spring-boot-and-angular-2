b45d5a8530bfeefd4843b4cd326a9db06e666bf4
binhnguyennus <binhnguyenvnuhcm@gmail.com>
2017-12-24 14:54:29 +0800

    Config the app

--- myapp/src/main/java/com/benny/myapp/MyappApplication.java
+++ myapp/src/main/java/com/benny/myapp/MyappApplication.java
@@ -1,9 +1,13 @@
 package com.benny.myapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class MyappApplication {
 
 	public static void main(String[] args) {
